b73591130ceda82c4517fc6ba545fec304dc404a
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2026-05-05 09:43:06 +0400

    commented tracing span for later use

--- src/order_book/tracing.rs
+++ src/order_book/tracing.rs
@@ -1,70 +1,70 @@
-use tracing::{Span, info_span};
-use tracing::field::Empty;
+// use tracing::{Span, info_span};
+// use tracing::field::Empty;
 
-pub struct Tracing {}
+// pub struct Tracing {}
 
-impl Tracing {
-    pub fn match_order_span(
-        order_id: u64,
-        filled: Empty,
-        reason: Empty,
-        order_type: &'static str,
-        is_buy_side: bool,
-        levels_consumed: Empty,
-        orders_touched: Empty,
-        actual_time : Empty
-    ) -> Span {
-        info_span!("match_order", order_id = %order_id,
-                    filled = filled,
-                    reason = reason,
-                    order_type = %order_type ,
-                    is_buy_side = %is_buy_side,
-                    levels_consumed = levels_consumed,
-                    orders_touched = orders_touched,
-                    actual_time = actual_time
-        )
-    }
-    pub fn modify_span(
-        order_id: u64,
-        filled: bool,
-        reason: Empty,
-        modify_reason: Empty,
-        intermediate_error : Empty,
-        order_type: &'static str,
-        is_buy_side: bool,
-        levels_consumed: u32,
-        orders_touched: u32,
-    ) -> Span {
-        info_span!("modify", order_id = %order_id,
-                    filled = %filled,
-                    reason = reason,
-                    modify_reason = modify_reason,
-                    intermediate_error = intermediate_error,
-                    order_type = %order_type ,
-                    is_buy_side = %is_buy_side,
-                    levels_consumed = %levels_consumed,
-                    orders_touched = %orders_touched
-        )
-    }
+// impl Tracing {
+//     pub fn match_order_span(
+//         order_id: u64,
+//         filled: Empty,
+//         reason: Empty,
+//         order_type: &'static str,
+//         is_buy_side: bool,
+//         levels_consumed: Empty,
+//         orders_touched: Empty,
+//         actual_time : Empty
+//     ) -> Span {
+//         info_span!("match_order", order_id = %order_id,
+//                     filled = filled,
+//                     reason = reason,
+//                     order_type = %order_type ,
+//                     is_buy_side = %is_buy_side,
+//                     levels_consumed = levels_consumed,
+//                     orders_touched = orders_touched,
+//                     actual_time = actual_time
+//         )
+//     }
+//     pub fn modify_span(
+//         order_id: u64,
+//         filled: bool,
+//         reason: Empty,
+//         modify_reason: Empty,
+//         intermediate_error : Empty,
+//         order_type: &'static str,
+//         is_buy_side: bool,
+//         levels_consumed: u32,
+//         orders_touched: u32,
+//     ) -> Span {
+//         info_span!("modify", order_id = %order_id,
+//                     filled = %filled,
+//                     reason = reason,
+//                     modify_reason = modify_reason,
+//                     intermediate_error = intermediate_error,
+//                     order_type = %order_type ,
+//                     is_buy_side = %is_buy_side,
+//                     levels_consumed = %levels_consumed,
+//                     orders_touched = %orders_touched
+//         )
+//     }
 
-    pub fn cancel_span(
-        order_id: u64,
-        success_status: bool,
-        reason: &'static str,
-    ) -> Span{
-        info_span!("cancel", order_id = %order_id,
-                    success_status = %success_status,
-                    reason = %reason,
-        )
-    }
-    pub fn depth_span(
-        security_id: Empty,
-        status: Empty,
-        reason: Empty,
-    ) -> Span{
-        info_span!("depth", security_id = security_id,
-                    status = status,
-                    reason = reason,
-        )
-    }
-}
+//     pub fn cancel_span(
+//         order_id: u64,
+//         success_status: bool,
+//         reason: &'static str,
+//     ) -> Span{
+//         info_span!("cancel", order_id = %order_id,
+//                     success_status = %success_status,
+//                     reason = %reason,
+//         )
+//     }
+//     pub fn depth_span(
+//         security_id: Empty,
+//         status: Empty,
+//         reason: Empty,
+//     ) -> Span{
+//         info_span!("depth", security_id = security_id,
+//                     status = status,
+//                     reason = reason,
+//         )
+//     }
+// }
