476991f90429b7fb89be91d4652a9da5350c13fd
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2026-05-05 09:43:36 +0400

    commented tracing module

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,4 @@ pub mod order_book;
 pub use order_book::orderbook::{OrderBook, HalfBook};
 pub use order_book::matching_engine::MatchingEngine;
 pub use order_book::types::{EngineNewOrder, EngineModifyOrder, EngineCancelOrder};
-pub use order_book::tracing::Tracing;
+// pub use order_book::tracing::Tracing;
